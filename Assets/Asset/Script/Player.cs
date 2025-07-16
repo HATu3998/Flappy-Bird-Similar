ed82360b69d9f20b4d405ba2f9e5da40b6ee633c
Tus <3artemis98@gmail.com>
2025-07-17 06:18:42 +0700

    va cham

--- Assets/Asset/Script/Player.cs
+++ Assets/Asset/Script/Player.cs
@@ -1,4 +1,5 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
@@ -9,10 +10,13 @@ public class Player : MonoBehaviour
     Vector3 direction;
     public Text scoreText;
 
+    public static int PlayerHeath = 3;
+    public Slider heathBar;
+
    public int score = 0;
     void Start()
     {
-        
+        heathBar.value = PlayerHeath;
     }
 
     // Update is called once per frame
@@ -27,9 +31,28 @@ public class Player : MonoBehaviour
         direction.y += Gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        score++;
-        scoreText.text = "Score :" + score;
+        Debug.Log("Va chạm với: " + col.gameObject.tag);
+        if (col.CompareTag("Point"))
+        {
+            score++;
+            scoreText.text = "Score :" + score;
+        }
+
+        //if (col.gameObject.name.Contains("log"))
+        //{
+            
+        //}
+        else if (col.CompareTag("Obstacle"))
+        {
+            if(PlayerHeath <= 0)
+            {
+                Time.timeScale = 0;
+            }
+            PlayerHeath--;
+            SceneManager.LoadScene("SampleScene");
+        }
+       
     }
 }
